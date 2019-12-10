2fd3c2f5b1544da6c0d3cefd2b96746d6cfa193b
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-12-10 23:09:22 +0000

    print random numbers and label them if positive, zero or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,5 +6,5 @@ if number > 0:
     print("{} is positive".format(number))
 elif number == 0:
     print("{} is Zero".format(number))
-elif number < 0:
+else number < 0:
     print("{} is negative".format(number))
