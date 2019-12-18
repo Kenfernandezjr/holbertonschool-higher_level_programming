046d3fde172bb047030d7e2a278aec06c1bbe5b4
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-12-18 05:53:16 +0000

    replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,14 +2,13 @@
 
 def new_in_list(my_list, idx, element):
 
-    new = my_list.copy()
-
     if idx < 0:
-        return(new)
+        return(my_list)
 
-    elif idx > len(new):
+    elif len(my_list) > idx:
+        new = my_list.copy()
+        new[idx] = element
         return(new)
 
     else:
-        new[idx] = element
-        return(new)
+        return(my_list)
