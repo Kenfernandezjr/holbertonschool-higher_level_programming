c1da4a4de3f049474e384a558c23dc1710629a78
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-12-18 07:55:20 +0000

    print maximum integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,9 @@ def max_integer(my_list=[]):
 
     if not my_list:
         return None
-    for i in (my_list):
+    new = my_list.copy()
+    for i in (new):
 
-        my_list.sort()
+        new.sort()
 
-    return(my_list[-1])
+    return(new[-1])
