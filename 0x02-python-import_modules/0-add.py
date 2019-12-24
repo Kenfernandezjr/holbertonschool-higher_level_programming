e5c2c1d94ced822f9c6f2f4e25db579229465cd0
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-12-24 22:06:53 +0000

    Adding two variables to create the sum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    a = 1
+    b = 2
+    c = add(a, b)
 
-print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, c))
