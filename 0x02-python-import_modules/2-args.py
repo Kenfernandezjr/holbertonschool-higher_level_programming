016e4d45e74c8a6afeacaa816cbe92326168cff8
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-12-26 18:50:01 +0000

    created a parse for arguments and counter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("0 arguments.")
 
     elif len(argv[1:]) == 1:
-        print("{:d} arguments:".format(len(argv[1:])))
+        print("{:d} argument:".format(len(argv[1:])))
 
     else:
         print("{:d} arguments:".format(len(argv[1:])))
