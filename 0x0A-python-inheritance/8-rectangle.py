3fb1ffed612139f046e0788c6d466454c3184590
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-21 05:35:52 +0000

    Instantiation with width and height: def __init__(self, width, height):

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+7-base_geometry.py Module
+"""
+
+
+class BaseGeometry:
+    """
+    class
+    """
+    def area(self):
+        """
+        public instance method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer validator
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+            """
+            Subclass created
+            """
+
+class Rectangle(BaseGeometry):
+    """
+    inherit class
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
