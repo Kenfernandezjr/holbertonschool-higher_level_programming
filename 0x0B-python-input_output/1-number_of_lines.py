017cb77ec7eb8c2bb0cb033e23202750249fcc83
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-22 00:54:30 +0000

    number of lines in the txt.file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,15 +2,8 @@
 def number_of_lines(filename=""):
     with open(filename, encoding="utf-8") as myFile:
 
-        lineNumber = 1
-
-        while True:
-
-            line = myFile.readlines()
-
-            if not line:
-                break
-
-            for x in line:
-                lineNumber += 1
-            return lineNumber
+        lineNumber = 0
+        line = myFile.readlines()
+        for x in line:
+            lineNumber += 1
+        return lineNumber
