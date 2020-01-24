483210b586cc027d952b2dab23e5c6df4c9da13b
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-24 00:13:21 +0000

    uploading test for checking positive numbers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     if isinstance(value, int):
         try:
             print("{:d}".format(value))
-            return(True)
         except Exception:
             return(False)
+        return(True)
