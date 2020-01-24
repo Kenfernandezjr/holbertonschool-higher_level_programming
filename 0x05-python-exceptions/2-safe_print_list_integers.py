5e497bdd53c774d00f111a6d83ac2f426a254db9
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-24 01:07:06 +0000

    update pass to exception, swap continue

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     index = 0
+
     for index in range(x):
         try:
             print("{:d}".format(my_list[index]), end="")
             index += 1
-        except (ValueError, TypeError):
-            continue
-    print()
+        except(TypeError, ValueError):
+            pass
+    print("")
     return(index)
