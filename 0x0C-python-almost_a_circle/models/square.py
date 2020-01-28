13bca20f27c3b8dde411f5725a7d97a3fbdae224
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-28 02:20:56 +0000

    update square class get and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
-
+""" Square class/Module """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Square class """
     def __init__(self, size, x=0, y=0, id=None):
-            super().__init__(size, size, x, y, id)
+        """ init with super inheritance """
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ str for Square """
         return str("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """ getter for size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ setter for size """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
