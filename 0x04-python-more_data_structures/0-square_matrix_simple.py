857d9a3c249f13f1812870d0b22271ed4b3d8736
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-02-01 06:00:21 +0000

    Upload Square to Pow of 2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for x in range(matrix):
-        print('{}'.append(x**2))
+    result = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result[i][j] += pow(matrix[i][j], 2)
+
+    for n in result:
+        return(result)
