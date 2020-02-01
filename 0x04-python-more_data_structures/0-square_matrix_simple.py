6867d391cbabd91245e923d07c83649abc6974d6
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-02-01 07:53:47 +0000

    Upload Square test #2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            result[i][j] += pow(matrix[i][j], 2)
-
-    for n in result:
-        return(result)
+    new_matr = []
+    for n in matrix:
+        new_matr.append(list(map(lambda x: x ** 2, n)))
+    return new_matr
