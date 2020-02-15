678a079760c18a8bcd04df79284df1a8a88986d5
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-02-15 01:36:34 +0000

    Update base json #1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Json list_dictories """
         if list_dictionaries is None:
