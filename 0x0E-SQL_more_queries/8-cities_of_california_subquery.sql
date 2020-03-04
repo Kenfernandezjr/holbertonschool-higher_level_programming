d051a018862bf3d099716e348d0ae2f3ef21ee6f
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-04 03:14:48 +0000

    Upload test #1 see id in asecending order

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California that can be found
-SELECT id, name FROM cities
+SELECT id FROM cities
 WHERE state_id=(
-SELECT id, name FROM states
+SELECT id FROM states
 WHERE 'California' )
 ORDER BY cities.id ASC;
