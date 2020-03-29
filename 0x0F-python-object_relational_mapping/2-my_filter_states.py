cfb261760f43cb7fe4307abe0686af1e82e89a66
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-29 05:18:25 +0000

    Upload test #1, Take in an argument to find a certian search parameter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+show created table with states list with n
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    '''
+    if the main is not being executed
+    '''
+    mydb = MySQLdb.connect(host='localhost',
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    '''
+    database connection
+    '''
+    mycursor = mydb.cursor()
+    '''
+    cursor
+    '''
+    sql = ("SELECT * FROM states WHERE name Like '{}' ORDER by name ASC".format(sys.argv[4]))
+    '''
+    sql variable
+    '''
+    mycursor.execute(sql)
+    '''
+    execute sql code
+    '''
+    myresult = mycursor.fetchall()
+    '''
+    fetching the results
+    '''
+    for x in myresult:
+        print(x)
+
+    '''
+    traversal
+    '''
