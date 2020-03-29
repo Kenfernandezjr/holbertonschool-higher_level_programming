9804d180f728a9285e79ddda31f5cdb4d603facd
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-29 04:26:00 +0000

    Upload test #1, locate  state with the same first letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+show created table with states list with n
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    '''
+    if the main is not being executed
+    '''
+    mydb = MySQLdb.connect(host='localhost',
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    '''
+    database connection
+    '''
+    mycursor = mydb.cursor()
+    '''
+    cursor
+    '''
+    sql = "SELECT * FROM states ORDER BY states.id ASC"
+    '''
+    sql variable
+    '''
+    mycursor.execute(sql)
+    '''
+    execute sql code
+    '''
+    myresult = mycursor.fetchall()
+    '''
+    fetching the results
+    '''
+    for x in myresult:
+        if x[1][0] == 'N':
+            print(x)
+
+    '''
+    traversal
+    '''
