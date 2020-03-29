08b3c3b3362363f2dbd6bcde40c634ca724cd093
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-29 09:22:47 +0000

    Upload test # 1, concat with seperator for combined output

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+show created table with states list with n
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    '''
+    if the main is not being executed
+    '''
+    mydb = MySQLdb.connect(host='localhost',
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    '''
+    database connection
+    '''
+    mycursor = mydb.cursor()
+    '''
+    cursor
+    '''
+    prime = sys.argv[4]
+    '''
+    variable for input argument
+    '''
+    mycursor.execute(
+        """SELECT GROUP_CONCAT(cities.name
+        SEPARATOR ', ')
+        FROM cities
+        JOIN states ON states.id = cities.state_id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC""", (prime,))
+
+    '''
+    execute sql code
+    '''
+    myresult = mycursor.fetchall()
+    '''
+    fetching the results
+    '''
+    for row in myresult:
+        print(row[0])
+
+    mydb.close()
+
+    '''
+    traversal
+    '''
