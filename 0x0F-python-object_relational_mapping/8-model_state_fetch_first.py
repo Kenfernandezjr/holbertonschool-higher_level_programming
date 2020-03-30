da38e56568b131cd6c1b2a000e4b7aa7208c5f07
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-30 06:10:54 +0000

    Upload test #1, Print the first row

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+list first  State objects
+'''
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    for u in session.query(State).order_by((State.id)).limit(1).all():
+        print("{}: {}".format(u.id, u.name))
+
+    session.close()
