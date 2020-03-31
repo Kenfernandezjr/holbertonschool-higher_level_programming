60eb3c4d86c063a4617c1559320fd9b998953b8a
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-31 05:01:09 +0000

    Upload test #1, modify id on table States

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+State object with the name
+'''
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    Mex1 = session.query(State).filter(State.id ='2').first()
+    Mex1.name = 'New Mexico'
+    session.commit()
+    session.close()
