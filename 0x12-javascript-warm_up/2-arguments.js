c2124ca16f37c86eec58c8f48ccb50485169e2ea
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-06 00:52:49 +0000

    Take in arguments and return no arguments or arguments found

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length < 3) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
+}
