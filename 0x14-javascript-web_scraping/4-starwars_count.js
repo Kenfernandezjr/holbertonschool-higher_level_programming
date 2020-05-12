a92e044946d3de8baa449d1ceae15e805d861d41
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-12 20:21:29 +0000

    Test #1

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request
+  .get(process.argv[2],
+    function (error, response, text) {
+      if (error) {
+	    console.log(error);
+      } else {
+	    const d = JSON.parse(text);
+	    let g = 0;
+	    for (let i = 0; i < d.results.length; i++) {
+          const characters = d.results[i].characters;
+          if (characters.filter(str => str.includes('people/18/')).length !== 0) {
+	      g++;
+          }
+	    }
+	  console.log(g);
+      }
+    });
