56c917fd1ea87c2e6eb31acf2586b99c62bfd0ae
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-12 21:15:17 +0000

    Test #2, fixed semistandard errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,15 @@
 
 const request = require('request');
 
-request
-  .get(process.argv[2],
-    function (error, response, text) {
-      if (error) {
-	    console.log(error);
-      } else {
-	    const d = JSON.parse(text);
-	    let g = 0;
-	    for (let i = 0; i < d.results.length; i++) {
-          const characters = d.results[i].characters;
-          if (characters.filter(str => str.includes('people/18/')).length !== 0) {
-	      g++;
-          }
-	    }
-	  console.log(g);
-      }
-    });
+request.get(process.argv[2], function (error, response, text) {
+  if (error) throw error;
+  const d = JSON.parse(text);
+  let g = 0;
+  for (let i = 0; i < d.results.length; i++) {
+    const characters = d.results[i].characters;
+    if (characters.filter(str => str.includes('people/18/')).length !== 0) {
+      g++;
+    }
+  }
+  console.log(g);
+});
