d5e69bf8a9d2225995aaff83169f3ee70d0e2cfa
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-13 04:36:15 +0000

    pushing 3 projects

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-let urls = 'https://swapi-api.hbtn.io/api/films/';
+const urls = 'https://swapi-api.hbtn.io/api/films/';
 const contact = process.argv[2];
 const res = urls.concat(contact);
 
 request.get(res, function (error, response, text) {
-    if (error) throw error;
-    console.log(JSON.parse(text).title);
+  if (error) throw error;
+  console.log(JSON.parse(text).title);
 });
