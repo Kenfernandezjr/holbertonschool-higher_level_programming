6c60ba61638eb3e4bb78c70003ab97ab722685f5
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-06-23 05:05:52 +0000

    script for header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" script to get X-request-id  """
+""" script to get X-request-id """
 
-import urllib.request
 from sys import argv
+import urllib.request
 
-
+if __name__ == "__main__":
 with urllib.request.urlopen(argv[1]) as response:
     r = response.info()
     print(r.get('X-Request-Id'))
