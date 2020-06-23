c3ac385a6b029811af9d2dbfe7ee51a3cd0f9016
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-06-23 13:55:08 +0000

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Displays body or error code"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    red = requests.get(url)
+    if red.status_code == 200:
+        print(red.text)
+    else:
+        print('Error code: {}'.format(r.status_code))
