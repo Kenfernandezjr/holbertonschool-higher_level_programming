c4b727e79ca110a99310164d89fe765eb6d11fed
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-06-23 05:17:24 +0000

    take an url and email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,8 +7,11 @@ import urllib.parse
 
 
 if __name__ == "__main__":
-    values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values).encode('ascii')
+    values = {
+        'email': argv[2]
+    }
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
 
     with urllib.request.urlopen(req) as response:
